089b1242a069708774e079400c7d3d0472aab4df
PinkiesRevenge <85567947+PinkiesRevenge@users.noreply.github.com>
2022-09-16 09:27:56 -0400

    improved and modified for better function
    dynamic delete, added complete, added IDs fixxed IDs etc...

--- alt.js
+++ alt.js
@@ -1,21 +1,38 @@
-let btnAdd=document.querySelector('button')
+let btnAdd=document.querySelector('#addTask')
 let table=document.querySelector('table')
 
-let nameInput=document.querySelector('#name')
+let taskInput=document.querySelector('#task')
 let ageInput=document.querySelector('#age')
-let countryInput=document.querySelector('#country')
+let commentInput=document.querySelector('#comment')
 
 btnAdd.addEventListener('click',() =>{
-    let name = nameInput.value
+    let task = taskInput.value
     let age = ageInput.value
-    let country = countryInput.value
+    let comment = commentInput.value
 
-    let template =`
+    table.innerHTML +=`
         <tr>
-            <td>${name}</td>
-            <td>${age}</td>
-            <td>${country}</td>
+            <td>${task}</td>
+            <!-- <td>${age}</td> -->
+            <td><p><button class="completeButton">Complete</button></p><p><button class="deleteButton">Delete</button></p></td>
+            <td>${comment}</td>
         </tr>
     `
-    table.innerHTML += template
+    function onDeleteRow(e){
+        if(!e.target.classList.contains("deleteButton")){
+            return;
+        }
+        const btn = e.target;
+        btn.closest('tr').remove();
+    };
+    function onCompleteRow(e){
+        if(!e.target.classList.contains("completeButton")){
+            return;
+        }
+        const btn = e.target;
+        btn.closest('tr').remove();
+    };
+    table.addEventListener("click",onCompleteRow);
+    table.addEventListener("click",onDeleteRow);
+
 })
